e6c71b99befcc177c59649d337def072e10d78d8
Rokan87 <rokananhasib@gmail.com>
2024-10-02 22:21:02 +0200

    Refactor user data handling in Home component, update TonConnectUI dependency, and implement wallet connection feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
     if (userData?.referralCode) {
       const referralLink = `https://t.me/mytestingsambot?start=${userData.referralCode}`;
       const message = `Hey! Te invito a aprender sobre la blockchain y cobrar por aprender: ${referralLink}`;
-
       window.open(`https://t.me/share/url?url=${encodeURIComponent(referralLink)}&text=${encodeURIComponent(message)}`, '_blank');
     }
   };
@@ -25,6 +24,7 @@ export default function Home() {
       const tonConnectUI = new TonConnectUI({
         manifestUrl: 'https://raw.githubusercontent.com/Rokan87/mitest/refs/heads/main/manifest.json'
       });
+
       const connection = await tonConnectUI.connectWallet();
       const walletAddress = connection.account.address;
 
@@ -32,7 +32,7 @@ export default function Home() {
       if (userData) {
         const updatedUserData = { ...userData, walletAddress };
         await saveOrUpdateUserInFirestore(updatedUserData, userData.referralCode);
-        setUserData(updatedUserData);
+        setUserData(updatedUserData);  // Actualiza el estado local con la nueva dirección de la billetera
       }
 
       console.log("Conectado a la billetera de Telegram:", walletAddress);
@@ -49,9 +49,12 @@ export default function Home() {
           const referralCode = "defaultReferralCode";  // Usa un código de referido predeterminado
           setUserData(user);
 
+          // Guarda o actualiza el usuario en Firestore
           await saveOrUpdateUserInFirestore(user, referralCode);
+
+          // Obtén los datos del usuario actualizados desde Firestore
           const userDataFromFirestore = await fetchUserData(user.id);
-          setUserData(userDataFromFirestore);
+          setUserData(userDataFromFirestore);  // Actualiza el estado local con los datos del usuario desde Firestore
         }
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -76,7 +79,10 @@ export default function Home() {
           <p>Puntos: {userData.points}</p>
           <p>Usuarios Invitados: {userData.invitedUsersCount}</p>
 
+          {/* Botón para invitar amigos */}
           <button onClick={inviteFriends}>Invitar a amigos</button>
+
+          {/* Mostrar dirección de la billetera o botón para conectar */}
           {userData.walletAddress ? (
             <p>Dirección de la billetera: {userData.walletAddress}</p>
           ) : (
